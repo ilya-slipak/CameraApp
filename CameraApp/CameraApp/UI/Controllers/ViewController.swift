8abfec776d8c27aba3edd9a38acbd041a6b6bc1d
Ilya Slipak <ilya.slipak@requestum.com>
2020-05-17 20:16:50 +0300

    initial commit

---
+++ CameraApp/CameraApp/UI/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Ilya Slipak on 17.05.2020.
+//  Copyright © 2020 Ilya Slipak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
